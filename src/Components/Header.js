745457c44c7ca5cec2ddb6b0daaf54e405f43131
Darren <hldaz77@gmail.com>
2023-08-07 17:23:39 +0100

    auth0 added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import Login from "../Login";
+import Logout from "../Logout";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      Header
+      <Login />
+      <Logout />
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 
 import { HelmetProvider } from "react-helmet-async";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <Auth0Provider
+        domain={process.env.REACT_APP_DOMAIN}
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
+      ,
     </HelmetProvider>
   </React.StrictMode>
 );
