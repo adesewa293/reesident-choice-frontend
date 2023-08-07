b14152d71559cd3f12771570cb8245a46ef25629
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-07 21:09:40 +0100

    update menu functionality

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
-import './Modal.css'; // Import your CSS file for styling
+import React, { useState } from "react";
+import "./Modal.css";
+import axios from "axios";
 
 function Modal() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [weeek, setWeek] = useState('');
-  const [day, setDay] = useState('');
-  const [mealtime, setMealTime] = useState('');
-  const [imageUrl, setImgUrl] = useState('');
+  const [week, setWeek] = useState("");
+  const [day, setDay] = useState("");
+  const [mealtime, setMealTime] = useState("");
+  const [imageUrl, setImgUrl] = useState("");
+  const [main_1, setMain1] = useState("");
+  const [main_2, setMain2] = useState("");
+  const [side_1, setSide1] = useState("");
+  const [side_2, setSide2] = useState("");
+  const [desert_1, setDesert1] = useState("");
+  const [error, setError] = useState("");
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -16,8 +23,29 @@ function Modal() {
     setIsModalOpen(false);
   };
 
-  const handleSave = () => {
-    closeModal();
+  const handleSave = async () => {
+    try {
+      const response = await axios.post(
+        `https://resident-choice-api.onrender.com/menus`,
+        {
+          week,
+          day,
+          mealtime,
+          imageUrl,
+          main_1,
+          main_2,
+          side_1,
+          side_2,
+          desert_1,
+        }
+      );
+      console.log("New menu added:", response.data);
+
+      closeModal();
+    } catch (error) {
+      console.log("error", error);
+      setError("Error adding new menuItem");
+    }
   };
 
   return (
@@ -26,11 +54,14 @@ function Modal() {
       {isModalOpen && (
         <div className="modal">
           <div className="modal-content">
-            <span className="close" onClick={closeModal}>&times;</span>
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            {error && <p>{error}</p> }
             <input
               type="text"
               placeholder="week"
-              value={weeek}
+              value={week}
               onChange={(e) => setWeek(e.target.value)}
             />
             <input
@@ -50,6 +81,36 @@ function Modal() {
               placeholder="imageUrl"
               value={imageUrl}
               onChange={(e) => setImgUrl(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="main1"
+              value={main_1}
+              onChange={(e) => setMain1(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="main2"
+              value={main_2}
+              onChange={(e) => setMain2(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="side1"
+              value={side_1}
+              onChange={(e) => setSide1(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="side2"
+              value={side_2}
+              onChange={(e) => setSide2(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="desert1"
+              value={desert_1}
+              onChange={(e) => setDesert1(e.target.value)}
             />
             <button onClick={handleSave}>Save</button>
           </div>
