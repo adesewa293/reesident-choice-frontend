cd567d83f21796d5525ccbe98a21603bf962f080
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-08-07 09:53:14 +0100

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,7 @@
 import React from 'react'
-import "./Header.css"
 
 export default function Header() {
   return (
-
-    <header>
-      <img src='' ClassName="logo" alt="logo"/>
-      <nav ClassName="Navheader">
-        <a href='/'>Home</a>
-        <a href='/'>About</a>
-        <a href='/'>Contact us</a>
-        <button ClassName="login-btn">Login</button>
-      </nav>
-    </header>
+    <div>Header</div>
   )
 }
