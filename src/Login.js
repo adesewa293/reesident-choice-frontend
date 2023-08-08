31bd900c3932807a30b045a67f0f8cf1c3ba121d
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-08 10:22:57 +0100

    add authtication logic

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return !isAuthenticated && <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
