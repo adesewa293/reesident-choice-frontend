9794917d2d4a5f7065dcc68799d4fceb6dbe7068
Raaqiyaa <135287211+Raaqiyaa@users.noreply.github.com>
2023-08-09 20:17:02 +0100

    update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -24,7 +24,6 @@ import "./Contact.css"
             <Row className="mb-5 mt-3">
             <Col lg="8">
                 <h1 className="display-4 mb-4">Contact Us</h1>
-                <hr className="t_border my-4 ml-0 text-left" />
             </Col>
             </Row>
             <Row className="sec_sp">
