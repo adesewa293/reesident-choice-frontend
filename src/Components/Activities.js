2111345298d8020e52f880d8128a69b193cf85ed
christol <christol.beresford@hotmail.co.uk>
2023-08-09 14:13:57 +0100

    photos added

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -1,7 +1,52 @@
 import React from 'react'
 
-export default function Activities() {
-  return (
+import photo1 from '../Images/Elderly_Bingo.jpg'
+import photo2 from '../Images/Elderly_Arts _and_Crafts.jpg'
+import photo3 from '../Images/Elderly_Baking_club.jpg'
+import photo4 from '../Images/Elderly_Card_making.jpeg'
+import photo5 from '../Images/Elderly_Cooking_classes.jpg'
+import photo6 from '../Images/Elderly_Dance_classes.jpeg'
+import photo7 from '../Images/Elderly_Gardening_club.jpg'
+import photo8 from '../Images/Elderly_memory_games.jpg'
+import photo9 from '../Images/Elderly_Movie_Night.jpeg'
+import photo10 from '../Images/Elderly_music_theapy.jpg'
+import photo11 from '../Images/Elderly_Painiting_Club.jpg'
+import photo12 from '../Images/Elderly_Quiz_night.jpg'
+import photo13 from '../Images/Elderly_Reading_aloud.jpg'
+import photo14 from '../Images/Elderly_Retro_party.jpg'
+import photo15 from '../Images/Elderly_Sewing_Class.jpg'
+import photo16 from '../Images/Elderly_Singing_club.jpg'
+import photo17 from '../Images/Elderly_walking_club.jpg'
+
+  export default function Activities() {
+
+return (
+  <div>
+    <img src={photo1}/>
+    <img src={photo2}/>
+    <img src={photo3}/>
+    <img src={photo4}/>
+    <img src={photo5}/>
+    <img src={photo6}/>
+    <img src={photo7}/>
+    <img src={photo8}/>
+    <img src={photo9}/>
+    <img src={photo10}/>
+    <img src={photo11}/>
+    <img src={photo12}/>
+    <img src={photo13}/>
+    <img src={photo14}/>
+    <img src={photo15}/>
+    <img src={photo16}/>
+    <img src={photo17}/>
+
     <div>Activities</div>
-  )
+  </div>
+    )
 }
+
+//Correct flow
+
+// later in your react (JSX)
+//HTML goes inside return 
+//Yellow but before return = any logic.
