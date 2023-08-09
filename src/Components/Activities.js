da8214375ac4d95f27eab3ef2e7db432bfd21a0c
christol <christol.beresford@hotmail.co.uk>
2023-08-09 17:08:34 +0100

    activites updated

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -1,3 +1,49 @@
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
+import "./Activities.css"
+
+
+export default function Activities() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    getActivities()
+    },[])
+
+
+  // THIS function is fetching all activites
+  async function getActivities() { 
+    let API = 'http://localhost:8086/get-week-events'
+    const result = await axios.get(API)
+    console.log(result)
+    setActivities(result.data)
+    console.log(activities)
+  }
+
+  return (
+    <div>
+      <h2>Activities to choose for today</h2>
+        <ul>
+          {activities.map(activity => {
+            return (
+              <>
+                <div>
+                <h2>{activity.description}</h2>
+                <p>{activity.summary}</p>
+                {/* <p>{activity.start}</p>
+                <p>{activity.end}</p> */}
+                </div>
+                <div>
+                  <img src={activity.attachments ? activity.attachments[0].fileUrl : ''} alt={'some text'}/>
+                </div>
+                <button></button>
+              </>
+            )})}
+        </ul>
+    </div>
+  )
+
+import "./Activities.css"
 import React from 'react'
 
 import photo1 from '../Images/Elderly_Bingo.jpg'
@@ -12,41 +58,24 @@ import photo9 from '../Images/Elderly_Movie_Night.jpeg'
 import photo10 from '../Images/Elderly_music_theapy.jpg'
 import photo11 from '../Images/Elderly_Painiting_Club.jpg'
 import photo12 from '../Images/Elderly_Quiz_night.jpg'
-import photo13 from '../Images/Elderly_Reading_aloud.jpg'
-import photo14 from '../Images/Elderly_Retro_party.jpg'
-import photo15 from '../Images/Elderly_Sewing_Class.jpg'
-import photo16 from '../Images/Elderly_Singing_club.jpg'
-import photo17 from '../Images/Elderly_walking_club.jpg'
 
   export default function Activities() {
 
 return (
-  <div>
-    <img src={photo1}/>
-    <img src={photo2}/>
-    <img src={photo3}/>
-    <img src={photo4}/>
-    <img src={photo5}/>
-    <img src={photo6}/>
-    <img src={photo7}/>
-    <img src={photo8}/>
-    <img src={photo9}/>
-    <img src={photo10}/>
-    <img src={photo11}/>
-    <img src={photo12}/>
-    <img src={photo13}/>
-    <img src={photo14}/>
-    <img src={photo15}/>
-    <img src={photo16}/>
-    <img src={photo17}/>
+  <div className='activitiesPhotos'>
+    <img src={photo1} alt = "bingo" />
+    <img src={photo2} alt = "Arts&crafts"/> 
+    <img src={photo3} alt = "Baking club"/>
+    <img src={photo4} alt = "Card making"/>
+    <img src={photo5} alt = "Cooking classes"/>
+    <img src={photo6} alt = "Gardening club"/>
+    <img src={photo7} alt = "Memory games"/>
+    <img src={photo8} alt = "Move night"/>
+    <img src={photo9} alt = "Music theapy"/>
+    <img src={photo10} alt = "Painting club"/>
+    <img src={photo11} alt = "Quiz night"/>
 
     <div>Activities</div>
   </div>
     )
-}
-
-//Correct flow
-
-// later in your react (JSX)
-//HTML goes inside return 
-//Yellow but before return = any logic.
+}}
